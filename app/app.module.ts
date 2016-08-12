5e1b63077a46e08f5b8d87e73ba4c2aa1665f7f2
Rodrigo Dias <rodrigo.dias@serpro.gov.br>
2016-08-12 15:38:34 -0300

    Routing - dashboard top heroes

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule }       from '@angular/platform-browser';
 import { FormsModule }         from '@angular/forms';
 
 import { AppComponent }        from './app.component';
+import { routing }             from './app.routing';
 import { HeroesComponent }     from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroService }         from './hero.service';
 
 @NgModule({
-    imports: [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+    imports: [ BrowserModule, FormsModule, routing ],
+    declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, DashboardComponent ],
     providers: [ HeroService ],
     bootstrap: [ AppComponent ]
 })
