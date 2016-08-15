085cb3f002e24460c80fcbea589da38655add3c6
Rodrigo Dias <rodrigo.dias@serpro.gov.br>
2016-08-15 11:14:50 -0300

    Routing - navigation

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,6 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent }      from './heroes.component';
 import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
 
 const appRoutes: Routes = [ {
     path: 'heroes',
@@ -12,6 +13,9 @@ const appRoutes: Routes = [ {
     path: '',
     redirectTo: '/dashboard',
     pathMatch: 'full'
-} ];
+}, {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+}, ];
 
 export const routing = RouterModule.forRoot(appRoutes);
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -13,4 +13,9 @@ export class HeroService {
             setTimeout(() => resolve(HEROES), 2000)
         );
     }
+
+    getHero(id: number) {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+    }
 }
